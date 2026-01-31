112bf3cd1e5ef94f2df365e49833666e760f0635
Karim Mostafa <karimmostafahq@outlook.com>
2026-01-31 14:31:36 +0200

    Create __init__.py

---
+++ data_core/__init__.py
@@ -0,0 +1,11 @@
+# Public contract exports (DO NOT BREAK)
+
+from .core import (
+    fetch_all_results,
+    store_simulation_result
+)
+
+__all__ = [
+    "fetch_all_results",
+    "store_simulation_result"
+]
